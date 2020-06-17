a376ff9f07bd2a350c5f3d18dc52c643018369ab
James Zhao <im.jameszhao@gmail.com>
2020-06-17 15:57:44 -0500

    add some flights operation

--- finalproject/airline-service/src/main/java/miu/edu/ea/airlineservice/service/FlightService.java
+++ finalproject/airline-service/src/main/java/miu/edu/ea/airlineservice/service/FlightService.java
@@ -2,8 +2,10 @@ package miu.edu.ea.airlineservice.service;
 
 import miu.edu.ea.airlineservice.domain.Flight;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface FlightService {
     List<Flight> findDepartureByCodeOrArrivalByCode(String DCode, String ACode);
+    List<Flight> findByAirportCode(String dCode, String aCode, Pageable pageable);
 }
