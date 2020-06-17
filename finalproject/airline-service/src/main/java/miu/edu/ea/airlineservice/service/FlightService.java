390f25a96fa628d3c0b1458e0bd9ea1fcdc622ff
James Zhao <im.jameszhao@gmail.com>
2020-06-17 15:59:05 -0500

    Revert "add some flights operation"
    This reverts commit a376ff9f07bd2a350c5f3d18dc52c643018369ab.

--- finalproject/airline-service/src/main/java/miu/edu/ea/airlineservice/service/FlightService.java
+++ finalproject/airline-service/src/main/java/miu/edu/ea/airlineservice/service/FlightService.java
@@ -2,10 +2,8 @@ package miu.edu.ea.airlineservice.service;
 
 import miu.edu.ea.airlineservice.domain.Flight;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface FlightService {
     List<Flight> findDepartureByCodeOrArrivalByCode(String DCode, String ACode);
-    List<Flight> findByAirportCode(String dCode, String aCode, Pageable pageable);
 }
--- finalproject/airline-service/src/main/java/miu/edu/ea/airlineservice/service/FlightServiceImpl.java
+++ finalproject/airline-service/src/main/java/miu/edu/ea/airlineservice/service/FlightServiceImpl.java
@@ -1,15 +1,10 @@
 package miu.edu.ea.airlineservice.service;
 
-import miu.edu.ea.airlineservice.domain.Airport;
 import miu.edu.ea.airlineservice.domain.Flight;
 import miu.edu.ea.airlineservice.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.criteria.*;
-import java.awt.print.Pageable;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,30 +17,5 @@ public class FlightServiceImpl implements FlightService {
     @Override
     public List<Flight> findDepartureByCodeOrArrivalByCode(String DCode, String ACode) {
         return flightRepository.findByDepartureOrArrival(DCode, ACode);
-    }
-
-    @Override
-    public List<Flight> findByAirportCode(String dCode, String aCode, Pageable pageable) {
-        List<Flight> flights = flightRepository.findAll(
-                new Specification<Flight>() {
-                    @Override
-                    public Predicate toPredicate(Root<Flight> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-                        List<Predicate> predicates = new ArrayList<>();
-
-                        if(dCode != null && !dCode.equals("")) {
-                            Join<Flight, Airport> join = root.join("departure", JoinType.INNER);
-                            predicates.add(criteriaBuilder.equal(join.get("code"), dCode));
-                        }
-
-                        if(aCode != null && aCode.equals("")) {
-                            Join<Flight, Airport> join = root.join("arrival", JoinType.INNER);
-                            predicates.add(criteriaBuilder.equal(join.get("code"), aCode));
-                        }
-
-                        return criteriaQuery.where(predicates.toArray(new Predicate[predicates.size()]))
-                                .getRestriction();
-                    }
-                });
-        return flights;
     }
 }
