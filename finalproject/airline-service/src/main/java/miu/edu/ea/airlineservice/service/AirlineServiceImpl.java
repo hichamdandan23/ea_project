781cf9b020bea35cd003ed9b987e2829b43ebf64
microgt <mgameal@gmail.com>
2020-06-17 15:45:45 -0500

    Created Admin CRUD

--- finalproject/airline-service/src/main/java/miu/edu/ea/airlineservice/service/AirlineServiceImpl.java
+++ finalproject/airline-service/src/main/java/miu/edu/ea/airlineservice/service/AirlineServiceImpl.java
@@ -1,12 +1,15 @@
 package miu.edu.ea.airlineservice.service;
 
 import miu.edu.ea.airlineservice.domain.Airline;
+import miu.edu.ea.airlineservice.domain.Airport;
 import miu.edu.ea.airlineservice.domain.Flight;
 import miu.edu.ea.airlineservice.repository.AirlineRepository;
 import miu.edu.ea.airlineservice.repository.FlightRepository;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AirlineServiceImpl implements AirlineService {
@@ -18,5 +21,49 @@ public class AirlineServiceImpl implements AirlineService {
     @Override
     public List<Airline> findByCode(String code) {
         return airlineRepository.findByCode(code);
+    }
+
+    @Override
+    public List<Airline> getAllAirlines() {
+        return airlineRepository.findAll();
+    }
+
+    @Override
+    public Airline getById(Long id) {
+        Airline tempLine = new Airline();
+        tempLine.setName("Airline Not Found!");
+        Optional<Airline> airForceOne = airlineRepository.findById(id);
+        if(airForceOne.isPresent()){
+            tempLine = airForceOne.get();
+        }
+        return tempLine;
+    }
+
+    @Override
+    public Airline createOrUpdate(Airline airline) {
+        Airline tempAir = airline;
+        Optional<Airline> airForceOne = airlineRepository.findById(airline.getId());
+        if(airForceOne.isPresent()) {
+            tempAir = airForceOne.get();
+
+            tempAir.setCode(airline.getCode());
+            tempAir.setName(airline.getName());
+            tempAir.setHistory(airline.getHistory());
+        }
+
+        airlineRepository.save(tempAir);
+
+        return airline;
+    }
+
+    @Override
+    public Boolean deleteById(Long id) {
+        boolean success = false;
+        Optional<Airline> airForceOne = airlineRepository.findById(id);
+        if(airForceOne.isPresent()){
+            airlineRepository.deleteById(id);
+            success = true;
+        }
+        return success;
     }
 }
